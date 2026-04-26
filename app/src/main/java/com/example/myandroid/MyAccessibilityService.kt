652aa4d3229c24578be74c8815cc1efbbc847212
GetyeTek <getyeteklu2@gmail.com>
2026-04-26 11:03:41 +0300

    Add FLAG_NOT_TOUCHABLE and FLAG_SHOW_WHEN_LOCKED to the blin

--- app/src/main/java/com/example/myandroid/MyAccessibilityService.kt
+++ app/src/main/java/com/example/myandroid/MyAccessibilityService.kt
@@ -58,8 +58,9 @@ class MyAccessibilityService : AccessibilityService() {
                 android.view.WindowManager.LayoutParams.MATCH_PARENT,
                 android.view.WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
                 android.view.WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or
                 android.view.WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
                 android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 android.graphics.PixelFormat.TRANSLUCENT
             )
