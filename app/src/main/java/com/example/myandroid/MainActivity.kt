7dd346b0ed184ff1f0ec1721df67853f193e85af
GetyeTek <getyeteklu2@gmail.com>
2026-04-26 12:30:40 +0300

    Implement 7-day Engagement Tracking and integrate into UI ho

--- app/src/main/java/com/example/myandroid/MainActivity.kt
+++ app/src/main/java/com/example/myandroid/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
+        EngagementTracker.recordEvent(this, "UI_OPEN")
         runPermissionCascade()
     }
 
