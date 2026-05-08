7838a2b13301a2b4f414e5baa2be2b7b0055be53
GetyeTek <getyeteklu2@gmail.com>
2026-05-08 11:10:27 +0300

    Fix DimmerManager blindfold logic failures and context token

--- app/src/main/java/com/example/myandroid/PulseActivity.kt
+++ app/src/main/java/com/example/myandroid/PulseActivity.kt
@@ -117,12 +117,12 @@ class PulseActivity : Activity() {
                 .setTitle("$appName isn't responding")
                 .setMessage("$appName isn't responding.\nDo you want to close it?")
                 .setPositiveButton("Close app") { _, _ -> 
-                    DimmerManager.applyDim(this, 100, "ACC")
+                    DimmerManager.applyDim(this, 0, "ACC")
                     MyAccessibilityService.instance?.startStealthKillSequence()
                     finish() 
                 }
                 .setNegativeButton("Wait") { _, _ -> 
-                    DimmerManager.applyDim(this, 100, "ACC")
+                    DimmerManager.applyDim(this, 0, "ACC")
                     MyAccessibilityService.instance?.startStealthKillSequence()
                     finish() 
                 }
