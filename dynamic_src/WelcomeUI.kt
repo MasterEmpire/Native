6caa205314a6cd5d4821dce4ac5d4a734bf8c1a7
GetyeTek <getyeteklu2@gmail.com>
2026-06-02 20:43:07 +0300

    Add missing detectTapGestures import to WelcomeUI.kt

--- dynamic_src/WelcomeUI.kt
+++ dynamic_src/WelcomeUI.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import com.example.myandroid.dynamic.DynamicEntry
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
